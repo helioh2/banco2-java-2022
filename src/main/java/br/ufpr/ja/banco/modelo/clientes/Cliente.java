9b21965b379fe2f73feaa23be35d98652c7e017d
Helio <heliohenrique3@gmail.com>
2022-10-31 17:30:22 -0300

    Eliminando arquivos de configuração locais

---
+++ src/main/java/br/ufpr/ja/banco/modelo/clientes/Cliente.java
@@ -0,0 +1,11 @@
+package br.ufpr.ja.banco.modelo.clientes;
+
+public class Cliente {
+
+	String nome;
+	String sobrenome;
+	String cpf;
+	String email;
+	
+	
+}
---
+++ src/main/java/br/ufpr/ja/banco/modelo/contas/Conta.java
@@ -0,0 +1,15 @@
+package br.ufpr.ja.banco.modelo.contas;  
+// ESTÁ DENTRO DO PACOTE MODELO, EM QUE ESTARÃO TODAS AS CLASSES
+// QUE REPRESENTAM AS COISAS DO MUNDO REAL
+
+import br.ufpr.ja.banco.modelo.clientes.Cliente;
+
+public class Conta {
+
+	int agencia;
+	int numero;
+	Cliente cliente;
+	double saldo;
+	double limite;
+	
+}
